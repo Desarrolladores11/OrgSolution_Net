30eff47ca4345a43ae986f1b319e02b5a47a3c25
gusmantilla <erwin.mantilla@gmail.com>
2022-08-26 20:56:48 -0500

    Se complementa el proyecto con la clases faltantes

--- Entidad/src/prueba_empresa.java
+++ Entidad/src/prueba_empresa.java
@@ -1,12 +1,32 @@
 public class prueba_empresa {
     public static void main(String[] args) {
-        empresa unaPrueba = new empresa();
-        System.out.println(unaPrueba);
+        empresa PrimeraPrueba = new empresa();
+        System.out.println(PrimeraPrueba);
 
-        unaPrueba.setDireccion("falsa calle");
-        unaPrueba.setNit("456");
-        unaPrueba.setNombre("11_desarrolladores");
-        unaPrueba.setTelefono("999");
-        System.out.println(unaPrueba);
+        PrimeraPrueba.setDireccion("falsa calle");
+        PrimeraPrueba.setNit("456");
+        PrimeraPrueba.setNombre("11_desarrolladores");
+        PrimeraPrueba.setTelefono("999");
+        System.out.println(PrimeraPrueba);
+
+        empleado SegundaPrueba = new empleado();
+        System.out.println(SegundaPrueba);
+
+        SegundaPrueba.setEmpresa("DESARROLLADORES");
+        SegundaPrueba.setCorreo("www.11_desarroladores.com");
+        SegundaPrueba.setRol("mantenimiento");
+        SegundaPrueba.setNombre_em("manuelito");
+        System.out.println(SegundaPrueba);
+
+        MovimientoDinero terceraPrueba = new MovimientoDinero();
+        System.out.println(terceraPrueba);
+
+        terceraPrueba.setMonto("14000");
+        terceraPrueba.setMonto_positivo("12000");
+        terceraPrueba.setMonto_negativo("-12000");
+        terceraPrueba.setConcepto("compra mouse");
+        terceraPrueba.setUsuario("ivancito");
+        System.out.println(SegundaPrueba);
+
     }
 }
