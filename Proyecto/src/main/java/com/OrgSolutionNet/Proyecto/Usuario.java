acb73afab650b6e7404bd936eb3e146714e1186d
Esteban2618 <estebanc2618@gmail.com>
2022-09-02 21:55:05 -0500

    Se reversa la clase usuario, Esteban

--- Proyecto/src/main/java/com/OrgSolutionNet/Proyecto/Usuario.java
+++ Proyecto/src/main/java/com/OrgSolutionNet/Proyecto/Usuario.java
@@ -2,48 +2,18 @@ package com.OrgSolutionNet.Proyecto;
 
 public class Usuario {
 
-<<<<<<< HEAD
-    private long id;
-    private String nombre;
-=======
     private String nombre_em;
->>>>>>> 91c6f0b8155495218a5a5d1ea4af83c50749d3c6
     private String email;
     private String empresaUsuario;
     private String rolUsuario;
 
     public Usuario() {
-<<<<<<< HEAD
-        this.id = 0001;
-        this.nombre = "Erika Carvajal";
-=======
         this.nombre_em = "Erika Carvajal";
->>>>>>> 91c6f0b8155495218a5a5d1ea4af83c50749d3c6
         this.email = "mirollo19@hot";
         this.empresaUsuario = "Alcaldia";
         this.rolUsuario = "Contratista";
     }
 
-<<<<<<< HEAD
-    public Usuario(long id, String nombre, String email, String empresaEmpleado, String rolEmpleado) {
-        this.id = id;
-        this.nombre = nombre;
-        this.email = email;
-        this.empresaUsuario = empresaUsuario;
-        this.rolUsuario = getRolUsuario();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-=======
     public Usuario(String nombre_em, String email, String empresaUsuario, String rolUsuario) {
         this.nombre_em = nombre_em;
         this.email = email;
@@ -53,7 +23,6 @@ public class Usuario {
 
     public String getNombre_em() {
         return nombre_em;
->>>>>>> 91c6f0b8155495218a5a5d1ea4af83c50749d3c6
     }
 
     public void setNombre_em(String nombre_em) {
